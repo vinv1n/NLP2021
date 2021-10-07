1f08ff21119457100de5fefbbf8df672441a7bcd
Juho Sipola <jusipola@student.oulu.fi>
2021-10-08 00:06:14 +0300

    Add web based semantic similarity

--- nlp/__main__.py
+++ nlp/__main__.py
@@ -4,6 +4,9 @@ import logging
 
 from .similarity import WebSimilarity
 
+logging.basicConfig(
+    format="%(asctime)s:%(name)s: %(message)s", level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
 
--- nlp/similarity.py
+++ nlp/similarity.py
@@ -2,11 +2,20 @@ import logging
 import os
 import requests
 import json
+import pprint
 
 from typing import Optional, Iterator, Tuple, Any, Dict, List
 
 
 logger = logging.getLogger(__name__)
+
+"""
+I don't explain here how to get these, please see links below
+
+https://developers.google.com/custom-search/v1/using_rest
+https://developers.google.com/custom-search/v1/introduction#identify_your_application_to_google_with_api_key
+https://stackoverflow.com/questions/4082966/what-are-the-alternatives-now-that-the-google-web-search-api-has-been-deprecated
+"""
 
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY", "")
 CX_ID = os.environ.get("GOOGLE_CX_ID", "")
@@ -14,7 +23,7 @@ CX_ID = os.environ.get("GOOGLE_CX_ID", "")
 
 def _permutate_words(word1: str, word2: str) -> Iterator[Tuple[str, str]]:
     for param1, param2 in zip((word1, word1, "",), (word2, "", word2,)):
-        yield f"{param1} {param2}"
+        yield f"{param1},{param2}"
 
 
 class WebSimilarity:
@@ -32,30 +41,45 @@ class WebSimilarity:
             logger.critical("Could not get page count, missing key")
             return []
 
-        request_url_base = f"https://www.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={CX_ID}:omuauf_lfve&q"
+        request_url_base = f"https://customsearch.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={CX_ID}&q"
 
         results = []
         for query in _permutate_words(word1, word2):
             request_url = f"{request_url_base}={query}"
             response = self.session.get(request_url)
             if response.status_code != 200:
-                logger.critical("Return code %s from google api, exiting", response.status_code)
+                logger.critical("Return code %s from google api %s", response.status_code, response.text)
                 return []
 
             try:
                 result = response.json()
-            except (ValueError, IOError) as e:
+            except (
+                ValueError,
+                IOError,
+                json.decoder.JSONDecodeError
+            ) as e:
                 logger.warning("Response from url %s was not json, error %s", request_url, e)
                 continue
 
-            logger.info("Recieved response with content %s from google search api %s", request_url, result)
+            logger.info("Recieved response with content %s from google search api", request_url)
+            if logger.isEnabledFor(logging.DEBUG):
+                logger.debug("Response from api %s", pprint.pformat(result))
 
-            # page results not yet computed as I am not sure what they contant
-            results.append(result)
+            try:
+                search_results = int(
+                    result.get("searchInformation", {}).get("totalResults", "0")
+                )
+            except ValueError:
+                search_results = 0
+
+            logger.info("With query %s found %s search results", query, search_results)
+            results.append(search_results)
 
         return results
 
     def web_similarity(self, word1: str, word2: str):
+        logger.info("Using words %s and %s for the search", word1, word2)
+
         lengths = self._get_search_pages(word1, word2)
         if not lengths:
             logger.warning("No results with words %s and %s", word1, word2)
@@ -66,7 +90,10 @@ class WebSimilarity:
             return 0.0
 
         try:
-            return lengths[0] / sum(lengths)
+            similarity = lengths[0] / sum(lengths)
         except ZeroDivisionError:
             logger.warning("Sum of page lenghts is 0, cannot compute similarity")
             return 0.0
+
+        logger.info("Similarity between word %s and %s is %s", word1, word2, similarity)
+        return similarity
